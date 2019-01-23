e630d330c60396a8cf714a59b6dca2f4202af3d3
EugeniaBaranova <21061997qwertyuiop@gmail.com>
2019-01-23 09:42:34 +0300

    edition cart

--- src/main/java/com/epam/web/controller/command/cart/ChangeCartItemCountCommand.java
+++ src/main/java/com/epam/web/controller/command/cart/ChangeCartItemCountCommand.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChangeCartItemCountCommand implements Command {
 
@@ -19,19 +20,39 @@ public class ChangeCartItemCountCommand implements Command {
     @Override
     public CommandResult execute(HttpServletRequest req, HttpServletResponse resp) throws ServiceException {
         String id = req.getParameter(RequestParameter.ID);
-
-        if (StringUtils.isNotEmpty(id)) {
-            if (StringUtils.isDigit(id)) {
+        String amountString = req.getParameter(RequestParameter.AMOUNT);
+        if (StringUtils.isNotEmpty(id) && StringUtils.isNotEmpty(amountString)) {
+            if (StringUtils.isDigit(id) && StringUtils.isDigit(amountString)) {
+                Integer newAmount = Integer.valueOf(amountString);
                 Long productId = Long.valueOf(id);
                 HttpSession session = req.getSession();
                 Object cartProducts = session.getAttribute(SessionAttribute.CART_PRODUCTS);
-                if(cartProducts != null){
-                    ((List<Long>)cartProducts)
-                            .add(Long.valueOf(productId));
+                if (cartProducts != null) {
+                    List<Long> cartProduct = (List<Long>) cartProducts;
+                    Integer existProductCount = getProductCount(productId, cartProduct);
+                    int deltaAmount = newAmount - existProductCount;
+                    if (deltaAmount > 0) {
+                        for (int i = 0; i < deltaAmount; i++) {
+                            cartProduct.add(productId);
+                        }
+                    } else if (deltaAmount < 0) {
+                        for (int i = 0; i < Math.abs(deltaAmount); i++) {
+                            cartProduct.remove(productId);
+                        }
+                    }
                     return CommandResult.forward(Pages.CART_PAGE);
                 }
             }
         }
         return CommandResult.forward(Pages.PAGE_NOT_FOUND);
+    }
+
+    private Integer getProductCount(Long id, List<Long> products) {
+        List<Long> matchProducts = products
+                .stream()
+                .filter(productId -> productId == id)
+                .collect(Collectors.toList());
+        return matchProducts.size();
+
     }
 }
